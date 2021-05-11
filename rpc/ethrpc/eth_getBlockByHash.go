9d0dbdfb95250a06236a37f5aa2a4ff4a50d11d0
jingxu85 <jing@sliver.tv>
2021-05-11 10:20:07 -0700

    Update eth_getBlockByHash.go
    follow the ETH convention

--- rpc/ethrpc/eth_getBlockByHash.go
+++ rpc/ethrpc/eth_getBlockByHash.go
@@ -1,7 +1,9 @@
 package ethrpc
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 
 	"github.com/thetatoken/theta-eth-rpc-adaptor/common"
 	tcommon "github.com/thetatoken/theta/common"
@@ -10,31 +12,69 @@ import (
 	rpcc "github.com/ybbus/jsonrpc"
 )
 
-type GetBlockArgs struct {
-	Hash string `json:"hash"`
+type Bytes8 [8]byte
+type Bytes256 [256]byte
+
+type eth_GetBlockResult struct {
+	Height    tcommon.JSONUint64 `json:"number"`
+	Hash      tcommon.Hash       `json:"hash"`
+	Parent    tcommon.Hash       `json:"parentHash"`
+	Timestamp *tcommon.JSONBig   `json:"timestamp"`
+	Proposer  tcommon.Address    `json:"miner"`
+	TxHash    tcommon.Hash       `json:"transactionsRoot"`
+	StateHash tcommon.Hash       `json:"stateRoot"`
+
+	Nonce           Bytes8             `json:"nonce"`
+	Sha3Uncles      tcommon.Hash       `json:"sha3Uncles"`
+	LogsBloom       Bytes256           `json:"logsBloom"`
+	Difficulty      tcommon.JSONUint64 `json:"difficulty"`
+	TotalDifficulty tcommon.JSONUint64 `json:"totalDifficulty"`
+	Size            tcommon.JSONUint64 `json:"size"`
+	GasLimit        tcommon.JSONUint64 `json:"gasLimit"`
+	GasUsed         tcommon.JSONUint64 `json:"gasUsed"`
+	ExtraData       []byte             `json:"extraData"`
+	Uncles          []tcommon.Hash     `json:"uncles"`
+	//TODO : add transactions
+	// Txs  []Tx         `json:"transactions"`
+
+	// ChainID   string             `json:"chain_id"`
+	// Epoch     tcommon.JSONUint64 `json:"epoch"`
+	// HCC                core.CommitCertificate   `json:"hcc"`
+	// GuardianVotes      *core.AggregatedVotes    `json:"guardian_votes"`
+	// EliteEdgeNodeVotes *core.AggregatedEENVotes `json:"elite_edge_node_votes"`
+	// Children []tcommon.Hash   `json:"children"`
+	// Status   core.BlockStatus `json:"status"`
 }
 
 // ------------------------------- eth_getBlockByHash -----------------------------------
-func (e *EthRPCService) GetBlockByHash(args *GetBlockArgs, resp *trpc.GetBlockResult) (result string, err error) {
+func (e *EthRPCService) GetBlockByHash(ctx context.Context, hashStr string, tx bool) (result eth_GetBlockResult, err error) {
 	logger.Infof("eth_getBlockByHash called")
 
 	client := rpcc.NewRPCClient(common.GetThetaRPCEndpoint())
-	rpcRes, rpcErr := client.Call("theta.GetBlock", trpc.GetBlockArgs{Hash: tcommon.HexToHash(args.Hash)})
+	rpcRes, rpcErr := client.Call("theta.GetBlock", trpc.GetBlockArgs{Hash: tcommon.HexToHash(hashStr)})
 
 	parse := func(jsonBytes []byte) (interface{}, error) {
 		trpcResult := trpc.GetBlockResult{}
 		json.Unmarshal(jsonBytes, &trpcResult)
 		return trpcResult, nil
 	}
-
+	result = eth_GetBlockResult{}
 	resultIntf, err := common.HandleThetaRPCResponse(rpcRes, rpcErr, parse)
 	if err != nil {
-		return "", err
+		return result, err
 	}
-	resultObj, err := json.Marshal(resultIntf)
-	if err != nil {
-		return "", err
+	theta_GetBlockResult, ok := resultIntf.(trpc.GetBlockResult)
+	if !ok {
+		return result, fmt.Errorf("failed to convert GetBlockResult")
 	}
-	result = string(resultObj)
+	result.Height = theta_GetBlockResult.Height
+	result.Hash = theta_GetBlockResult.Hash
+	result.Parent = theta_GetBlockResult.Parent
+	result.Timestamp = theta_GetBlockResult.Timestamp
+	result.Proposer = theta_GetBlockResult.Proposer
+	result.TxHash = theta_GetBlockResult.TxHash
+	result.StateHash = theta_GetBlockResult.StateHash
+	//TODO : handle tx
+	//TODO : handle other fields
 	return result, nil
 }
