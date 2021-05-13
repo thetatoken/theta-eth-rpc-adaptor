3b1a510d175fc90522d995e07e7ec962d0ef6bed
jingxu85 <jing@sliver.tv>
2021-05-12 23:06:42 -0700

    return false/status

--- rpc/ethrpc/eth_syncing.go
+++ rpc/ethrpc/eth_syncing.go
@@ -6,14 +6,24 @@ import (
 	"fmt"
 
 	"github.com/thetatoken/theta-eth-rpc-adaptor/common"
+	tcommon "github.com/thetatoken/theta/common"
 
 	trpc "github.com/thetatoken/theta/rpc"
 	rpcc "github.com/ybbus/jsonrpc"
 )
 
-// ------------------------------- eth_syncing -----------------------------------
+type ethSyncingResult struct {
+	StartingBlock tcommon.JSONUint64 `json:"startingBlock"`
+	CurrentBlock  tcommon.JSONUint64 `json:"currentBlock"`
+	HighestBlock  tcommon.JSONUint64 `json:"highestBlock"`
+}
+type syncingResultWrapper struct {
+	*ethSyncingResult
+	syncing bool
+}
 
-func (e *EthRPCService) Syncing(ctx context.Context) (result string, err error) {
+// ------------------------------- eth_syncing -----------------------------------
+func (e *EthRPCService) Syncing(ctx context.Context) (result interface{}, err error) {
 	logger.Infof("eth_syncing called")
 	result = "log4"
 	client := rpcc.NewRPCClient(common.GetThetaRPCEndpoint())
@@ -21,14 +31,31 @@ func (e *EthRPCService) Syncing(ctx context.Context) (result string, err error) 
 	parse := func(jsonBytes []byte) (interface{}, error) {
 		trpcResult := trpc.GetStatusResult{}
 		json.Unmarshal(jsonBytes, &trpcResult)
-		return trpcResult.Syncing, nil
+		re := syncingResultWrapper{&ethSyncingResult{}, false}
+		re.syncing = trpcResult.Syncing
+		if trpcResult.Syncing {
+			re.StartingBlock = 1
+			re.CurrentBlock = trpcResult.CurrentHeight
+			re.HighestBlock = trpcResult.LatestFinalizedBlockHeight
+		}
+		logger.Infof("jlog3 trpcResult %v, re", trpcResult, re)
+		return re, nil
 	}
 
 	resultIntf, err := common.HandleThetaRPCResponse(rpcRes, rpcErr, parse)
 	if err != nil {
 		return "", err
 	}
-	result = fmt.Sprintf("%v", resultIntf)
+	thetaSyncingResult, ok := resultIntf.(syncingResultWrapper)
+	if !ok {
+		return nil, fmt.Errorf("failed to convert syncingResultWrapper")
+	}
+	logger.Infof("jlog1 %v", thetaSyncingResult)
+	if !thetaSyncingResult.syncing {
+		result = false
+	} else {
+		result = thetaSyncingResult.ethSyncingResult
+	}
 
 	return result, nil
 }
