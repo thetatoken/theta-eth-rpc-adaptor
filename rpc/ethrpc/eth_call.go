12ed787a20c74e6b4cd4a09e2af1beb48ba168ad
zhenyang <zhenyang@sliver.tv>
2021-06-01 10:54:00 -0700

    Use new methods instead of duplicate code

--- rpc/ethrpc/eth_call.go
+++ rpc/ethrpc/eth_call.go
@@ -4,13 +4,10 @@ import (
 	"context"
 	"encoding/hex"
 	"encoding/json"
-	"math/big"
 
 	"github.com/thetatoken/theta-eth-rpc-adaptor/common"
-	"github.com/thetatoken/theta/cmd/thetacli/cmd/utils"
-	tcommon "github.com/thetatoken/theta/common"
-	"github.com/thetatoken/theta/ledger/types"
 
+	"github.com/thetatoken/theta/cmd/thetacli/cmd/utils"
 	trpc "github.com/thetatoken/theta/rpc"
 	rpcc "github.com/ybbus/jsonrpc"
 )
@@ -20,68 +17,23 @@ import (
 func (e *EthRPCService) Call(ctx context.Context, argObj common.EthSmartContractArgObj, tag string) (result string, err error) {
 	logger.Infof("eth_call called")
 
+	sctxBytes, err := common.GetSctxBytes(argObj)
+	if err != nil {
+		utils.Error("Failed to get smart contract bytes: %+v\n", argObj)
+		return result, err
+	}
+
 	client := rpcc.NewRPCClient(common.GetThetaRPCEndpoint())
 
-	fromAddress := argObj.From.String()
-
-	rpcRes, rpcErr := client.Call("theta.GetAccount", trpc.GetAccountArgs{Address: fromAddress})
+	rpcRes, rpcErr := client.Call("theta.CallSmartContract", trpc.CallSmartContractArgs{SctxBytes: hex.EncodeToString(sctxBytes)})
 
 	parse := func(jsonBytes []byte) (interface{}, error) {
-		trpcResult := trpc.GetAccountResult{Account: &types.Account{}}
-		json.Unmarshal(jsonBytes, &trpcResult)
-		return trpcResult.Account.Sequence, nil
-	}
-
-	resultIntf, err := common.HandleThetaRPCResponse(rpcRes, rpcErr, parse)
-
-	if err != nil {
-		return "", err
-	}
-	sequence := resultIntf.(uint64) + 1
-
-	from := types.TxInput{
-		Address: tcommon.HexToAddress(fromAddress),
-		Coins: types.Coins{
-			ThetaWei: new(big.Int).SetUint64(0),
-			TFuelWei: new(big.Int).SetUint64(common.Str2hex2unit(argObj.Value)),
-		},
-		Sequence: sequence,
-	}
-
-	to := types.TxOutput{
-		Address: tcommon.HexToAddress(argObj.To.String()),
-	}
-
-	gasPrice, ok := types.ParseCoinAmount(argObj.GasPrice)
-	if !ok {
-		utils.Error("Failed to parse gas price")
-	}
-
-	sctx := &types.SmartContractTx{
-		From:     from,
-		To:       to,
-		GasLimit: 500000,
-		GasPrice: gasPrice,
-		Data:     []byte(argObj.Data),
-	}
-
-	sctxBytes, err := types.TxToBytes(sctx)
-
-	if err != nil {
-		utils.Error("Failed to encode smart contract transaction: %v\n", sctx)
-	}
-
-	client = rpcc.NewRPCClient(common.GetThetaRPCEndpoint())
-
-	rpcRes, rpcErr = client.Call("theta.CallSmartContract", trpc.CallSmartContractArgs{SctxBytes: hex.EncodeToString(sctxBytes)})
-
-	parse = func(jsonBytes []byte) (interface{}, error) {
 		trpcResult := trpc.CallSmartContractResult{}
 		json.Unmarshal(jsonBytes, &trpcResult)
 		return trpcResult.VmReturn, nil
 	}
 
-	resultIntf, err = common.HandleThetaRPCResponse(rpcRes, rpcErr, parse)
+	resultIntf, err := common.HandleThetaRPCResponse(rpcRes, rpcErr, parse)
 	if err != nil {
 		return "", err
 	}
