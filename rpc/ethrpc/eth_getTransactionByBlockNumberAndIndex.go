931a8fc3f8bafd404cc41ffcca1d69d2e24ccaf8
zhenyang <zhenyang@sliver.tv>
2021-06-08 14:31:08 -0700

    Minor fix

--- rpc/ethrpc/eth_getTransactionByBlockNumberAndIndex.go
+++ rpc/ethrpc/eth_getTransactionByBlockNumberAndIndex.go
@@ -12,8 +12,8 @@ import (
 // ------------------------------- eth_getTransactionByBlockNumberAndIndex -----------------------------------
 func (e *EthRPCService) GetTransactionByBlockNumberAndIndex(ctx context.Context, numberStr string, txIndexStr string) (result common.EthGetTransactionResult, err error) {
 	logger.Infof("GetTransactionByBlockNumberAndIndex called")
-	height := GetHeightByTag(numberStr)
-	txIndex := GetHeightByTag(txIndexStr) //TODO: use common
+	height := common.GetHeightByTag(numberStr)
+	txIndex := common.GetHeightByTag(txIndexStr) //TODO: use common
 	client := rpcc.NewRPCClient(common.GetThetaRPCEndpoint())
 	rpcRes, rpcErr := client.Call("theta.GetBlockByHeight", trpc.GetBlockByHeightArgs{Height: height})
 	return GetIndexedTransactionFromBlock(rpcRes, rpcErr, txIndex)
