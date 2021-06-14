86ef45d3c08fe9a3120708302b5986f44169d9f3
zhenyang <zhenyang@sliver.tv>
2021-06-14 14:23:04 -0700

    Update net_version.go

--- rpc/netrpc/net_version.go
+++ rpc/netrpc/net_version.go
@@ -2,12 +2,48 @@ package netrpc
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/thetatoken/theta-eth-rpc-adaptor/common"
+	hexutil "github.com/thetatoken/theta/common/hexutil"
+	"github.com/thetatoken/theta/ledger/types"
+	trpc "github.com/thetatoken/theta/rpc"
+	rpcc "github.com/ybbus/jsonrpc"
 )
+
+type chainIDResultWrapper struct {
+	chainID string
+}
 
 // ------------------------------- net_version -----------------------------------
 
 func (e *NetRPCService) Version(ctx context.Context) (result string, err error) {
 	logger.Infof("net_version called")
+
+	client := rpcc.NewRPCClient(common.GetThetaRPCEndpoint())
+	rpcRes, rpcErr := client.Call("theta.GetStatus", trpc.GetStatusArgs{})
+	parse := func(jsonBytes []byte) (interface{}, error) {
+		trpcResult := trpc.GetStatusResult{}
+		json.Unmarshal(jsonBytes, &trpcResult)
+		re := chainIDResultWrapper{
+			chainID: trpcResult.ChainID,
+		}
+		return re, nil
+	}
+
+	resultIntf, err := common.HandleThetaRPCResponse(rpcRes, rpcErr, parse)
+	if err != nil {
+		return "", err
+	}
+	thetaChainIDResult, ok := resultIntf.(chainIDResultWrapper)
+	if !ok {
+		return "", fmt.Errorf("failed to convert chainIDResultWrapper")
+	}
+
+	thetaChainID := thetaChainIDResult.chainID
+	ethChainID := types.MapChainID(thetaChainID).Uint64()
+	result = hexutil.EncodeUint64(ethChainID)
 
 	return result, nil
 }
