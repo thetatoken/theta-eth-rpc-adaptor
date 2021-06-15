b15c7d06aebaefe41c8057df328e80a1f47600d4
zhenyang <zhenyang@sliver.tv>
2021-06-15 13:24:21 -0700

    Fix block height 0 case

--- rpc/ethrpc/eth_get_block_by_number.go
+++ rpc/ethrpc/eth_get_block_by_number.go
@@ -13,6 +13,15 @@ import (
 func (e *EthRPCService) GetBlockByNumber(ctx context.Context, numberStr string, txDetails bool) (result common.EthGetBlockResult, err error) {
 	logger.Infof("eth_getBlockByNumber called")
 	height := common.GetHeightByTag(numberStr)
+
+	if height == 0 {
+		height, err = common.GetCurrentHeight()
+
+		if err != nil {
+			return result, err
+		}
+	}
+
 	client := rpcc.NewRPCClient(common.GetThetaRPCEndpoint())
 	rpcRes, rpcErr := client.Call("theta.GetBlockByHeight", trpc.GetBlockByHeightArgs{
 		Height: height})
