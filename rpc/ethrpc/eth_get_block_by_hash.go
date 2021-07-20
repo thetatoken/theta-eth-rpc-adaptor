a19f2de9a6523e07ae36c9a72ac353402f95988d
zhenyang <zhenyang@sliver.tv>
2021-07-19 17:10:19 -0700

    Add coinbase tx detail in get block APIs

--- rpc/ethrpc/eth_get_block_by_hash.go
+++ rpc/ethrpc/eth_get_block_by_hash.go
@@ -29,23 +29,34 @@ func GetBlockFromTRPCResult(rpcRes *rpcc.RPCResponse, rpcErr error, txDetails bo
 		json.Unmarshal(jsonBytes, &trpcResult)
 		// logger.Infof("transactions count %d \n", len(trpcResult.Txs))
 		result.Transactions = make([]interface{}, len(trpcResult.Txs))
-		var objmap map[string]json.RawMessage
-		json.Unmarshal(jsonBytes, &objmap)
-		if objmap["transactions"] != nil {
-			var txmaps []map[string]json.RawMessage
-			json.Unmarshal(objmap["transactions"], &txmaps)
-			for i, omap := range txmaps {
-				if types.TxType(trpcResult.Txs[i].Type) == types.TxSmartContract {
-					if txDetails {
+		if txDetails {
+			var objmap map[string]json.RawMessage
+			json.Unmarshal(jsonBytes, &objmap)
+			if objmap["transactions"] != nil {
+				var txmaps []map[string]json.RawMessage
+				json.Unmarshal(objmap["transactions"], &txmaps)
+				for i, omap := range txmaps {
+					tx := common.EthGetTransactionResult{}
+					if types.TxType(trpcResult.Txs[i].Type) == types.TxSmartContract {
 						scTx := types.SmartContractTx{}
 						json.Unmarshal(omap["raw"], &scTx)
 						result.Transactions[i] = scTx
+						result.GasUsed = hexutil.Uint64(trpcResult.Txs[i].Receipt.GasUsed)
+					} else if types.TxType(trpcResult.Txs[i].Type) == types.TxSend {
+						sTx := types.SendTx{}
+						json.Unmarshal(omap["raw"], &sTx)
+						result.Transactions[i] = sTx
+					} else if types.TxType(trpcResult.Txs[i].Type) == types.TxCoinbase {
+						cTx := types.CoinbaseTx{}
+						json.Unmarshal(omap["raw"], &cTx)
+						tx.From = cTx.Proposer.Address
+						tx.Gas = hexutil.Uint64(0)
+						tx.Value = hexutil.Uint64(cTx.Proposer.Coins.TFuelWei.Uint64())
+						tx.Input = "0x"
+						data := cTx.Proposer.Signature.ToBytes()
+						GetRSVfromSignature(data, &tx)
+						result.Transactions[i] = tx
 					}
-					result.GasUsed = hexutil.Uint64(trpcResult.Txs[i].Receipt.GasUsed)
-				} else if txDetails && types.TxType(trpcResult.Txs[i].Type) == types.TxSend {
-					sTx := types.SendTx{}
-					json.Unmarshal(omap["raw"], &sTx)
-					result.Transactions[i] = sTx
 				}
 			}
 		}
@@ -64,12 +75,17 @@ func GetBlockFromTRPCResult(rpcRes *rpcc.RPCResponse, rpcErr error, txDetails bo
 	result.TxHash = theta_GetBlockResult.TxHash
 	result.StateHash = theta_GetBlockResult.StateHash
 	for i, tx := range theta_GetBlockResult.Txs {
-		if txDetails && (types.TxType(tx.Type) == types.TxSmartContract || types.TxType(tx.Type) == types.TxSend) {
+		if txDetails && (types.TxType(tx.Type) == types.TxSmartContract || types.TxType(tx.Type) == types.TxSend || types.TxType(tx.Type) == types.TxCoinbase) {
 			//already handled
 		} else {
 			result.Transactions[i] = tx.Hash
 		}
 	}
 	result.GasLimit = 20000000
+	result.LogsBloom = "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+	result.ExtraData = "0x"
+	result.Nonce = "0x0000000000000000"
+	result.Uncles = []tcommon.Hash{}
+
 	return result, nil
 }
