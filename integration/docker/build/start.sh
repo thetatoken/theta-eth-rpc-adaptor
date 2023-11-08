37d037b7b93f5cab2e1e365333d8d9a0d7913815
Jieyi Long (THETA Network) <jieyi@sliver.tv>
2023-11-08 14:09:36 -0800

    Fixes for Linux cross-compilation on Apple Silicon

--- integration/docker/build/start.sh
+++ integration/docker/build/start.sh
@@ -7,7 +7,7 @@ set -x
 
 GOBIN=/usr/local/go/bin/go
 
-$GOBIN build -o ./build/linux/theta-eth-rpc-adaptor ./cmd/theta-eth-rpc-adaptor
+CGO_ENABLED=1 GOARCH=amd64 CC=x86_64-linux-gnu-gcc CXX=g++-x86-64-linux-gnu $GOBIN build -o ./build/linux/theta-eth-rpc-adaptor ./cmd/theta-eth-rpc-adaptor
 
 set +x 
 
