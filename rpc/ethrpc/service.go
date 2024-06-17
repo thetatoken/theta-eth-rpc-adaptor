3f21357dc5e41656b84c97edf02c4562347bbe0a
Jieyi Long (THETA Network) <jieyi@sliver.tv>
2024-06-17 21:55:26 +0800

    Return errors when there are too many pending heavy getLogs queries

--- rpc/ethrpc/service.go
+++ rpc/ethrpc/service.go
@@ -1,10 +1,13 @@
 package ethrpc
 
 import (
+	"context"
+	"sync"
 	"time"
 
 	erpclib "github.com/ethereum/go-ethereum/rpc"
 	log "github.com/sirupsen/logrus"
+	"github.com/thetatoken/theta/common/timer"
 )
 
 var blockInterval time.Duration = 6 * time.Second
@@ -13,6 +16,15 @@ var logger *log.Entry = log.WithFields(log.Fields{"prefix": "ethrpc"})
 
 // EthRPCService provides an API to access to the Eth endpoints.
 type EthRPCService struct {
+	pendingHeavyGetLogsCounter           uint64
+	pendingHeavyGetLogsCounterLock       *sync.Mutex
+	pendingHeavyGetLogsCounterResetTimer *timer.RepeatTimer
+
+	// Life cycle
+	wg      *sync.WaitGroup
+	ctx     context.Context
+	cancel  context.CancelFunc
+	stopped bool
 }
 
 // NewEthRPCService creates a new API for the Ethereum RPC interface
@@ -21,10 +33,35 @@ func NewEthRPCService(namespace string) erpclib.API {
 		namespace = "eth"
 	}
 
+	serv := &EthRPCService{
+		wg: &sync.WaitGroup{},
+
+		pendingHeavyGetLogsCounter:           0,
+		pendingHeavyGetLogsCounterLock:       &sync.Mutex{},
+		pendingHeavyGetLogsCounterResetTimer: timer.NewRepeatTimer("pendingHeavyGetLogsCounterReset", 5*time.Minute),
+	}
+	serv.pendingHeavyGetLogsCounterResetTimer.Reset()
+
+	serv.wg.Add(1)
+	go serv.heavyQueryCounterLoop()
+
 	return erpclib.API{
 		Namespace: namespace,
 		Version:   "1.0",
-		Service:   &EthRPCService{},
+		Service:   serv,
 		Public:    true,
+	}
+}
+
+func (serv *EthRPCService) heavyQueryCounterLoop() {
+	defer serv.wg.Done()
+
+	for {
+		select {
+		case <-serv.pendingHeavyGetLogsCounterResetTimer.Ch:
+			serv.pendingHeavyGetLogsCounterLock.Lock()
+			serv.pendingHeavyGetLogsCounter = 0 // reset the counter to zero at a fixed time interval, otherwise the counter could get stuck if some pending queries never return
+			serv.pendingHeavyGetLogsCounterLock.Unlock()
+		}
 	}
 }
